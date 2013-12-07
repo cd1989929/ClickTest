532d7e6a199c78f85e35a32d9c9fbdeaeb4d10e4
dongdong <249016429@qq.com>
2013-12-07 12:11:57 +0800

    add scope how use

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+var app = angular.module('myApp',[]);
+app.run(function($rootScope){
+    $rootScope.name="Ari Lerner";
+    //alert('123');
+});
 
+app.controller('MyController',['$scope',function($scope){
+   $scope.person={
+       name:"chendong"
+   };
+}]);
+
+app.controller('ParentController',function($scope){
+    $scope.person={greeted:false};
+    });
+app.controller('ChildController',function($scope){
+    $scope.sayHello=function(){
+        $scope.person.greeted=true;
+    };
+});
